a2ef6804468ca20d6b7861c6e5e46e11c6a2b0d2
miaocansky <1535474309@qq.com>
2022-05-06 16:50:35 +0800

    增加权限管理 casbin

---
+++ casbin/casbin.go
@@ -0,0 +1,76 @@
+package casbin
+
+import (
+	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/miaocansky/go-tool/casbin/dto"
+	"gorm.io/gorm"
+	"strconv"
+	"sync"
+)
+
+var (
+	enforcer *casbin.Enforcer
+	once     sync.Once
+)
+
+type CasbinUtil struct {
+	ModelPath string
+	Db        *gorm.DB
+	Enforcer  *casbin.Enforcer
+}
+
+func NewCasbinUtil(modelPath string, db *gorm.DB) (*CasbinUtil, error) {
+	util := &CasbinUtil{
+		ModelPath: modelPath,
+		Db:        db,
+	}
+	_, err := util.GetEnforcer()
+	return util, err
+}
+
+//单列模式生成 Enforcer
+func (casbinUtil *CasbinUtil) GetEnforcer() (*casbin.Enforcer, error) {
+	//modelPath:="./resource/casbin/rbac_model.conf"
+	var err error
+	once.Do(func() {
+		a, _ := gormadapter.NewAdapterByDB(casbinUtil.Db)
+		enforcer, err = casbin.NewEnforcer(casbinUtil.ModelPath, a)
+	})
+	casbinUtil.Enforcer = enforcer
+	return enforcer, err
+
+}
+
+func (casbinUtil *CasbinUtil) LoadPolicy() {
+	//casbinUtil.GetEnforcer()
+	casbinUtil.Enforcer.LoadPolicy()
+
+}
+
+func (casbinUtil *CasbinUtil) AddPolicy(authorityId uint64, casbinInfos []dto.CasbinInfo) bool {
+	var policyLists [][]string
+	if casbinInfos == nil {
+		return false
+	}
+	for _, info := range casbinInfos {
+		authorityIdStr := strconv.FormatUint(authorityId, 10)
+		policyLists = append(policyLists, []string{authorityIdStr, info.Path, info.Method})
+	}
+	isAdd, _ := casbinUtil.Enforcer.AddPolicies(policyLists)
+	return isAdd
+}
+func (casbinUtil *CasbinUtil) DeleteAllPolicy() bool {
+	return casbinUtil.DeletePolicy(0)
+}
+
+func (casbinUtil *CasbinUtil) DeletePolicyForAuthorityId(authorityId uint64) bool {
+	authorityIdStr := strconv.FormatUint(authorityId, 10)
+	return casbinUtil.DeletePolicy(0, authorityIdStr)
+}
+
+func (casbinUtil *CasbinUtil) DeletePolicy(v int, p ...string) bool {
+	isDel, _ := casbinUtil.Enforcer.RemoveFilteredPolicy(v, p...)
+	return isDel
+}
---
+++ casbin/go.mod
@@ -0,0 +1,10 @@
+module github.com/miaocansky/go-tool/casbin
+
+go 1.16
+
+require (
+	github.com/casbin/casbin/v2 v2.45.0
+	github.com/casbin/gorm-adapter/v3 v3.6.0
+	github.com/go-sql-driver/mysql v1.6.0
+	gorm.io/gorm v1.23.5
+)
